6fb610242e5a819aa4476e8b5a72c67519ed4d03
haybuka <paschal.nduleo@gmail.com>
2021-05-14 15:27:17 +0100

    completed slider,and responsive layou

---
+++ assets/scripts/index.js
@@ -0,0 +1,43 @@
+let bodyWidth = document.querySelector('body').clientWidth;
+
+    $('.customer--stories').slick(slickers());
+
+function slickers(){
+  console.log(bodyWidth)
+  if (bodyWidth <= 1002){
+    return {
+      centerMode: false,
+      centerPadding: '20px',
+      slidesToShow: 2,
+      breakpoint : 1000,
+      arrows:false,
+      autoplay:true,
+      responsive: [
+        {
+          breakpoint: 670,
+          settings: {
+            arrows: false,
+            centerMode: false,
+            centerPadding: '0px',
+            slidesToShow: 2,
+            autoplaySpeed:1500,
+            cssEase:'ease-out'
+          }
+        },
+        {
+          breakpoint: 554,
+          settings: {
+            arrows: false,
+            centerMode: true,
+            centerPadding: '10px',
+            slidesToShow: 1,
+            autoplaySpeed:1500,
+            cssEase:'ease-out'
+          }
+        }
+      ]
+    }
+  }else{
+    return 'unslick'
+  }
+}
